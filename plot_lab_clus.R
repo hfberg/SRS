d561411fb8b0446a31c86108137f26fb305b12b3
Hanna F. Berg <hannaf.berg@hotmail.com>
2020-05-13 16:02:55 +0200

    Added mapped colors to ggplot. Fixed invalid graphics issue

--- plot_lab_clus.R
+++ plot_lab_clus.R
@@ -15,13 +15,24 @@ PC_y = 4 # the number of the PC you want to plot on the y-axis
 viz_leg<-as.data.frame(cbind(PCAs[,PC_x],PCAs[,PC_y]))
 
 # re-cluster based on new PCs?
-k_for_plot <- kmeans(viz_leg, 12, nstart=25, iter.max=1000)
+k_for_plot <- kmeans(viz_leg, 18, nstart=25, iter.max=1000)
 viz_leg<-as.data.frame(cbind(viz_leg,k_for_plot[["cluster"]]))
 
 viz_leg<-cbind(viz_leg,customer_lables[,leg])
 viz_leg[,3]<-as.factor(viz_leg[,3])
 viz_leg[,4]<-as.factor(viz_leg[,4])
 colnames(viz_leg)<-c(colnames(PCAs[PC_x]),colnames(PCAs[PC_y]),"Clusters",colnames(customer_lables[leg]))
+
+
+### create and map colors to clusters
+library(RColorBrewer)
+n <- 60
+qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
+col_vector = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
+pie(rep(1,n), col=sample(col_vector, n))
+
+names(col_vector)<-levels(viz_leg$Clusters)
+colScale <- scale_colour_manual(name = "Clusters",values = col_vector)
 
 
 
@@ -37,22 +48,26 @@ plot_leg_clust = ggplot() +
   
   #plot ellipses
   stat_ellipse(show.legend = T, aes(x = viz_leg[,1], y= viz_leg[,2], color = as.factor(viz_leg[,3])), data = viz_leg,inherit.aes = FALSE) +
-  scale_color_brewer(name = colnames(viz_leg[3]), palette="Paired") +
+  colScale +
+  #scale_color_brewer(name = colnames(viz_leg[3]), palette="Paired") +
   
   # set lables
   labs(title = "Clusters and lable of choice plotted",x = colnames(viz_leg[1]), y=colnames(viz_leg[2]))
 plot_leg_clust
 
 
+#two plots beside each other
 par(mfrow=c(1,2),mai=c(0.7,0.7,0.5,0.5))
 counts <- table(viz_leg[,4], viz_leg[,3])
 
+#barplot
 barplot(counts,  cex.names = 1,main=colnames(viz_leg[4]), col= as.factor(rownames(counts)),
         beside=TRUE,xlab="")
 legend("topleft", legend = rownames(counts), fill =as.factor(rownames(counts)), x.intersp=0.3, horiz=F,cex=0.5)
 title(xlab="Cluster index", line=2, cex.lab=0.8)
 
-plot(x=viz_leg[,1], y=viz_leg[,2],xlab="",ylab="",col=viz_leg[,3],main= "Clusters", pch=16)
+#scatter of clusters
+plot(x=viz_leg[,1], y=viz_leg[,2],xlab="",ylab="",col=as.factor(viz_leg[,3]),main= "Clusters", pch=16)
 legend("topright",legend = colnames(counts),x.intersp=0.3, fill =as.factor(colnames(counts)),  horiz=F,cex=0.5)
 title(ylab=colnames(viz_leg[2]), line=2, cex.lab=0.8)
 title(xlab=colnames(viz_leg[1]), line=2, cex.lab=0.8)
