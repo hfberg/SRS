61651afb9dc0d983ef47908be5fc759a84184dbd
Hanna F. Berg <hannaf.berg@hotmail.com>
2020-05-13 14:33:18 +0200

    adapting to data with imputed NA-values

--- Clustering.R
+++ Clustering.R
@@ -1,92 +1,92 @@
-
-# Create a correlation matrix from NKI data
-
-#Originally from website: https://towardsdatascience.com/market-segmentation-with-r-pca-k-means-clustering-part-1-d2c338b1dd0b
-# And website: https://www.r-bloggers.com/pca-and-k-means-clustering-of-delta-aircraft/
-
-# Modified by Hanna Berg 23-04-2020
-
-#Library for formatting the code.
-library(bannerCommenter)
-
-banner("Import and check data", emph = TRUE)
-
-#Load library to import excel file
-library(xlsx)
-
-# Give the full path to the data file to import, and which work sheet to use. 
-# Using windows be sure to change backslash from \ to / in path.
-raw <- read.xlsx("C:/Users/habe/Documents/NKI_clustering/2019_Raw_Data_NKI_scale10.xlsx",1, header = T)
-sum_raw<-as.data.frame(summary(raw))
-#optional, to check data
-#str(raw)
-#head(raw)
-
-#########################################
-
-# banner("Check and remove matrix for NAs", emph = TRUE)
-
-# NAs_in_raw<-as.data.frame(colSums(is.na(raw))) #Checking NAs 
 # 
-# # Remove all columns containing at least one NA 
-# # (temporary solution, some answers could maybe be approximated later on.)
+# # Create a correlation matrix from NKI data
 # 
-# rm_cols<-vector()
+# #Originally from website: https://towardsdatascience.com/market-segmentation-with-r-pca-k-means-clustering-part-1-d2c338b1dd0b
+# # And website: https://www.r-bloggers.com/pca-and-k-means-clustering-of-delta-aircraft/
 # 
-# for (a in (1:NROW(NAs_in_raw))){
-#   if(NAs_in_raw[a,] !=0) {
-#     rm_cols <- append(rm_cols, a)
+# # Modified by Hanna Berg 23-04-2020
+# 
+# #Library for formatting the code.
+# library(bannerCommenter)
+# 
+# banner("Import and check data", emph = TRUE)
+# 
+# #Load library to import excel file
+# library(xlsx)
+# 
+# # Give the full path to the data file to import, and which work sheet to use. 
+# # Using windows be sure to change backslash from \ to / in path.
+# raw <- read.xlsx("C:/Users/habe/Documents/NKI_clustering/2019_Raw_Data_NKI_scale10.xlsx",1, header = T)
+# sum_raw<-as.data.frame(summary(raw))
+# #optional, to check data
+# #str(raw)
+# #head(raw)
+# 
+# #########################################
+# 
+# # banner("Check and remove matrix for NAs", emph = TRUE)
+# 
+# # NAs_in_raw<-as.data.frame(colSums(is.na(raw))) #Checking NAs 
+# # 
+# # # Remove all columns containing at least one NA 
+# # # (temporary solution, some answers could maybe be approximated later on.)
+# # 
+# # rm_cols<-vector()
+# # 
+# # for (a in (1:NROW(NAs_in_raw))){
+# #   if(NAs_in_raw[a,] !=0) {
+# #     rm_cols <- append(rm_cols, a)
+# #   }
+# # }
+# # 
+# # raw[,rm_cols]<- NULL
+# # 
+# # # Remove troublesome columns, adapted especially for NKI data, 
+# # # disable or customize for other data matrices.
+# # raw[,1:2]<-NULL
+# # raw[,78]<- NULL
+# # 
+# # #remove unncessary variables, cuz we like a clean environment.
+# # rm(a,rm_cols)
+# 
+# ############################################
+# 
+# banner("replace NAs in matrix", emph =T)
+# raw_t<-raw
+# for (r in (1:nrow(raw))){
+#   for (c in (1:ncol(raw))){
+#     if (is.na(raw[r,c])){
+#       raw_t[r,c]<-round(rowMeans(raw, na.rm = T))[r]
+#     }
 #   }
 # }
 # 
-# raw[,rm_cols]<- NULL
+# View(raw_t) #inspect the new matrix
 # 
-# # Remove troublesome columns, adapted especially for NKI data, 
-# # disable or customize for other data matrices.
-# raw[,1:2]<-NULL
-# raw[,78]<- NULL
+# raw<-raw_t
+# rm(raw_t,c,r)
 # 
-# #remove unncessary variables, cuz we like a clean environment.
-# rm(a,rm_cols)
-
-############################################
-
-banner("replace NAs in matrix", emph =T)
-raw_t<-raw
-for (r in (1:nrow(raw))){
-  for (c in (1:ncol(raw))){
-    if (is.na(raw[r,c])){
-      raw_t[r,c]<-round(rowMeans(raw, na.rm = T))[r]
-    }
-  }
-}
-
-View(raw_t) #inspect the new matrix
-
-raw<-raw_t
-rm(raw_t,c,r)
-
-# Scaling is needed if: 
-#     - the units are different between columns
-#     - the means are very different between columns
-#     - the standard deviations are very different between columns
-
-# Here is code to calculate and plot mean and sd for each column.
-banner("Mean and standard deviation", emph = TRUE)
-
-mean_in_raw <- c()
-sd_in_raw <-c()
-
-for ( i in raw[,1:ncol(raw)]){
-  
-  mean_in_raw <- append(mean_in_raw,(mean(i)))
-  sd_in_raw <- append(sd_in_raw, (sd(i)))
-}
-
-plot(mean_in_raw,xlab= "question index")
-plot(sd_in_raw, xlab= "Question index")
-
- ######################### Start here for ready made data
+# # Scaling is needed if: 
+# #     - the units are different between columns
+# #     - the means are very different between columns
+# #     - the standard deviations are very different between columns
+# 
+# # Here is code to calculate and plot mean and sd for each column.
+# banner("Mean and standard deviation", emph = TRUE)
+# 
+# mean_in_raw <- c()
+# sd_in_raw <-c()
+# 
+# for ( i in raw[,1:ncol(raw)]){
+#   
+#   mean_in_raw <- append(mean_in_raw,(mean(i)))
+#   sd_in_raw <- append(sd_in_raw, (sd(i)))
+# }
+# 
+# plot(mean_in_raw,xlab= "question index")
+# plot(sd_in_raw, xlab= "Question index")
+# 
+#  ######################### Start here for ready made data
 
 banner("Correlation matrix", emph = TRUE)
 
@@ -157,7 +157,7 @@ plot(1:15, predict_no_clust, type="b", xlab="Number of Clusters",
 # Determine number of clusters from scree plot. 
 # Where does the elbow occur?
 # Apply k-means with k=elbow number
-k <- kmeans(PCAs, 8, nstart=25, iter.max=1000)
+k <- kmeans(PCAs, 12, nstart=25, iter.max=1000)
 library(RColorBrewer)
 library(scales)
 palette(alpha(brewer.pal(9,'Set1'), 0.5))
@@ -169,30 +169,30 @@ library("rgl")
 
 plot3d(PCAs, col=k$clust, pch=16)
 
-# Generates a non interactive plot called "plot"
-snapshot3d("plot.png")
-#postscript("C:/Users/habe/Documents/NKI_clustering/plot")
-
-
-# Generates a website with an interactive plot.
-writeWebGL(dir = "webGL", filename = file.path("webGL/index.html"))
-browseURL(
-  paste("file://", writeWebGL(dir=file.path(tempdir(), "webGL"), 
-                              width=500), sep="")
-)
-
-
-
-banner("Lable customers", emph = TRUE)
-
-#Load lables
-customer_lables = read.xlsx(file = "NKI_legends.xlsx",1)
-
-plot(PCAs[,1:2], col = customer_lables[,4], pch=16)
-legend("topright",legend = unique(customer_lables[,4]),  fill = c("blue", "red") )
-
-## With ggplot
-PCAs_t3<-cbind(PCAs_t,customer_lables[,3])
-
-# A basic scatterplot with color depending on Species
-ggplot(PCAs_t3[,1:2], aes(x=PCAs_t3[,1], y=PCAs_t3[,2], color=PCAs_t3[,3])) + geom_point(size=1) 
+# # Generates a non interactive plot called "plot"
+# snapshot3d("plot.png")
+# #postscript("C:/Users/habe/Documents/NKI_clustering/plot")
+# 
+# 
+# # Generates a website with an interactive plot.
+# writeWebGL(dir = "webGL", filename = file.path("webGL/index.html"))
+# browseURL(
+#   paste("file://", writeWebGL(dir=file.path(tempdir(), "webGL"), 
+#                               width=500), sep="")
+# )
+# 
+# 
+# 
+# banner("Lable customers", emph = TRUE)
+# 
+# #Load lables
+# customer_lables = read.xlsx(file = "NKI_legends.xlsx",1)
+# 
+# plot(PCAs[,1:2], col = customer_lables[,4], pch=16)
+# legend("topright",legend = unique(customer_lables[,4]),  fill = c("blue", "red") )
+# 
+# ## With ggplot
+# PCAs_t3<-cbind(PCAs_t,customer_lables[,3])
+# 
+# # A basic scatterplot with color depending on Species
+# ggplot(PCAs_t3[,1:2], aes(x=PCAs_t3[,1], y=PCAs_t3[,2], color=PCAs_t3[,3])) + geom_point(size=1) 
