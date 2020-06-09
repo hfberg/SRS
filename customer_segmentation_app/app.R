667d6ffa43fc428120cfe406d7f699e43ea4f088
Hanna F. Berg <hannaf.berg@hotmail.com>
2020-06-09 10:41:28 +0200

    Cleaned up the code

--- customer_segmentation_app/app.R
+++ customer_segmentation_app/app.R
@@ -1,19 +1,7 @@
-# This is a Shiny web application. You can run the application by clicking
+# A shiny web application for clustering of NKI data. 
+# HAnna F. Berg for SRS 2020-06-09
 
-# the 'Run App' button above.
-
-#
-
-# Find out more about building applications with Shiny here:
-
-#
-
-#    http://shiny.rstudio.com/
-
-#
-
-
-
+# Load libraries
 library(shiny)
 library(xlsx)
 library(scales)
@@ -22,75 +10,57 @@ library(ggplot2)
 library(ggnewscale)
 library(stringr)
 
-
-# Define UI for application that draws a histogram
-
-# k-means only works with numerical variables,
-
-# so don't give the user the option to select
-
-# a categorical variable
-
-
+# Load and shape PC data
 PCs <- xlsx::read.xlsx("PCs.xlsx",1)
-#PCs[,1]<- NULL
 names(PCs) <- str_replace_all(names(PCs), pattern = "[.]", replacement = " ")
 vars <- names(PCs)
 
+# Load and shape legends data
 customer_labels <- xlsx::read.xlsx("customer_labels.xlsx", 1)
-customer_labels[,1] <- NULL
 names(customer_labels) <- str_replace_all(names(customer_labels), pattern = "[.]", replacement = " ")
 for (i in 1:ncol(customer_labels)){
   customer_labels[,i] <- as.factor(customer_labels[,i])
 }
 
 
+################################################# GUI part
 
 ui <-pageWithSidebar(
   
   headerPanel('Customer segmentation NKI data'),
-  
   sidebarPanel(
-    
     selectInput('xcol', 'X Variable', names(PCs)),
-    
     selectInput('ycol', 'Y Variable', names(PCs), selected = names(PCs)[[2]]),
-    
     sliderInput("clusters", "Number of clusters:", min = 1, max = 30, value = 3),
-    
     selectInput('legend', 'Choose Legend', names(customer_labels), selected = names(customer_labels)[[6]])
     
-    
   ),
-
 
   mainPanel(
     
     textOutput("text"),
     plotOutput('plot1'),
-    plotOutput("barplot1"),
-    tableOutput("table"),
-    plotOutput("barplot2")
-
-
+    plotOutput("plot2"),
+    plotOutput("barplot")
     
   )
-  
 )
 
-###################################################################################################
-
-
+################################################# Code part
 
 server <- function(input, output, session) {
+
+################# Preparing for output
   
+# Create variables for x-axis   
   PC_x <-reactive(PCs[,input$xcol])
   PC_x_label <- reactive(input$xcol)
   
-  
+# Create variables for y-axis
   PC_y <- reactive(PCs[,input$ycol])
   PC_y_label <- reactive(input$ycol)
-  
+
+# Clustering on selected axes and save to variable
   clusters <- reactive({
     
     x = data.frame(PC_x(), PC_y())
@@ -99,25 +69,32 @@ server <- function(input, output, session) {
   })
 
   clus <- reactive(clusters()$cluster)
+  
+# Create legend variables
   labels <- reactive(customer_labels[,input$legend])
   n_labels <- reactive(names(customer_labels[,input$legend]))
 
-  n <- 60
-  qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
-  col_vector = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
-  #pie(rep(1,n), col=sample(col_vector, n))
+# create our own color chart  
+  n <- 30
+  qual_col_pals <- brewer.pal.info[brewer.pal.info$category == 'qual',]
+  col_vector <- unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
+  
+################# Generate output
   
   output$text <- renderText({
-  "test" # prit stuff here for easier visualization and error search.
+ # prit stuff here for easier visualization and error search.
   })
 
-  
-   output$plot1 <- renderPlot({
+
+# Plot legends with clusters circled
+  output$plot1 <- renderPlot({
     
-     names(col_vector)<-levels(clus())
-     colScale <- scale_colour_manual(name = "Clusters",values = col_vector)
+    # Preparation
+    names(col_vector)<-levels(clus())
+    colScale <- scale_colour_manual(name = "Clusters",values = col_vector)
      
-      plot_leg_clust = ggplot() +
+    # Plot
+    ggplot() +
        
        # scatter plot
        geom_point(alpha = 1,size=1.5, aes(x=PC_x(), y=PC_y(),color = labels())) +
@@ -127,51 +104,63 @@ server <- function(input, output, session) {
        new_scale_color() +
        
        #plot ellipses
-       stat_ellipse(aes(x = PC_x(), y= PC_y(), color = as.factor(clus()))) +
-       colScale +
+       stat_ellipse(aes(x = PC_x(), y= PC_y(), color = as.factor(clus()))) + colScale +
        
-       
-       # set labels
+       # set labels and themes
        labs(title = "Clusters and label of choice plotted",x = PC_x_label(), y=PC_y_label())+
        theme(panel.background = element_rect(fill = "white", color = "black"))
-     plot_leg_clust
    })
   
   
-##################### barplot
-
-  output$barplot1 <- renderPlot({
+# Plot only clusters
+  output$plot2 <- renderPlot({
   
+  #Preparation
     names(col_vector)<-levels(clus())
     colScale <- scale_colour_manual(name = "Clusters",values = col_vector)
-    
-    ggplot() + geom_point(alpha = 1,size=1.5, aes(x=PC_x(), y=PC_y(),color = as.factor(clus())), show.legend = T) +
-     colScale + 
+  
+  #Plot  
+    ggplot() +
+      
+      # scatter plot
+      geom_point(aes(x=PC_x(), y=PC_y(),color = as.factor(clus())), alpha = 1, size=1.5, show.legend = T) +
+     
+      # set color
+      colScale + 
+      
+      #set labels and themes
       labs(title = "Clustering based on PC of choice",x = PC_x_label(), y=PC_y_label(), color = "Clusters") +
       theme(panel.background = element_rect(fill = "white", color = "black"))
-    
     })  
   
-  output$barplot2 <- renderPlot({
+  
+# Plot barplot
+  output$barplot <- renderPlot({
+    
+    # Preparation
     counts_perc<-table(labels(), clus())
     counts<-as.data.frame(table(labels(), clus()))
     
-      for (i in 1:nrow(counts_perc)){
-         counts_perc[i,]<-round(counts_perc[i,]/sum(counts_perc[i,]),3)
-      }
+    for (i in 1:nrow(counts_perc)){
+       counts_perc[i,]<-round(counts_perc[i,]/sum(counts_perc[i,]),3)
+    }
+    
     counts_perc <- as.data.frame(counts_perc)
+    
+    # Plot
     ggplot(data=counts_perc[which(counts_perc$Freq>0),], aes(x=counts_perc[which(counts_perc$Freq>0),][,2], y=counts[which(counts$Freq>0),][,3], fill=counts_perc[which(counts_perc$Freq>0),][,1])) +
       geom_bar(stat="identity", position="fill") +
+      
+      # Plot percentages on bars
       geom_text(aes(label=percent(x=counts_perc[which(counts_perc$Freq>0),][,3], accuracy = 0.1)), vjust=1.6, color="black",
-                position = position_fill(0.9), size=2)+ 
-      theme(panel.background = element_rect(fill = "white")) +
-      labs(title = "Percentages of total label for each cluster",x = unique("Clusters"), y = "", fill = n_labels())
-    
-    #working
-      #barplot(counts_perc, legend.text = unique(labels()))
-    
+      position = position_fill(0.9), size=2)+
+      
+      # Plot labels and themes
+      labs(title = "Percentages of total label for each cluster",x = unique("Clusters"), y = "", fill = n_labels())+
+      theme(panel.background = element_rect(fill = "white"))
   })
   
 } 
 
+# Run app
 shinyApp(ui = ui, server = server)
